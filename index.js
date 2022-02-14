3763ce8be10bf70cd14b1108ce5dd855adba22ad
Daniel Escalona <daniel07escalona@gmail.com>
2022-02-14 13:57:31 -0500

    commit tree d778a1b3c2d1fe4b031095a07ba6f557805f06a6

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let count = parseInt(word.length/2, 10)
+  let length = word.length-1;
+
+  for( let i=0; i < count; i++){
+    if (word[i] !== word[length]){
+      return false
+    }
+    length--
+  }
+  return true
+
 }
 
 /* 
@@ -8,6 +19,7 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I use two variables that check the array from the beginning and the end. It's a quicker way to check the whole array. I divide the lenght by 2 and convert into an integer because words that even uneven number characters do no need to check the character that 'stays' in the middle, because it won't have a matching pair. You only need the check the two 'tails' of the word.
 */
 
 // You can run `node index.js` to view these console logs
